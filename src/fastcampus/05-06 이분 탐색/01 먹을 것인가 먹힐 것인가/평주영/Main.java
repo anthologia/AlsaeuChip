4e9a6e80ef81e959675955d2e509fed6e121711e
Anthologia <pyoungjy@gmail.com>
2022-10-06 14:12:58 +0900

    add: [이분 탐색] - 01 먹을 것인가 먹힐 것인가

--- src/fastcampus/05-06 이분 탐색/01 먹을 것인가 먹힐 것인가/평주영/Main.java
+++ src/fastcampus/05-06 이분 탐색/01 먹을 것인가 먹힐 것인가/평주영/Main.java
@@ -1,3 +1,53 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Integer[] A, B;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            A = makeArray(N);
+            B = makeArray(M);
+
+            Arrays.sort(A);
+            Arrays.sort(B, Comparator.reverseOrder());
+
+            int sum = calcFeedCnt();
+            sb.append(sum).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static Integer[] makeArray(int n) throws IOException {
+        Integer[] arr = new Integer[n];
+        String[] tmp = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(tmp[i]);
+        }
+        return arr;
+    }
+
+    public static int calcFeedCnt() {
+        int sum = 0;
+        for (int a : A) {
+            for (int i = 0; i < B.length; i++) {
+                if (a > B[i]) {
+                    sum += B.length - i;
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
 }
